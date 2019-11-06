6034f739e74ddda8332b8491dd6ba094e6ed92bf
SandyKio <sandykiozih@gmai.com>
2019-11-07 00:06:42 +0700

    Video 11

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,9 @@
 import 'package:blog_app/PhotoUpload.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'Authentication.dart';
+import 'Posts.dart';
 class HomePage extends StatefulWidget
 {
   HomePage({
@@ -17,6 +19,33 @@ class HomePage extends StatefulWidget
 }
 class _HomePageState extends State<HomePage>
 {
+  List<Posts> postList=[];
+  @override
+  void initState(){
+    super.initState();
+    DatabaseReference postsRef=FirebaseDatabase.instance.reference().child("Posts");
+    postsRef.once().then((DataSnapshoot snap){
+      var KEYS=snap.value.keys;
+      var DATA= snap.value;
+      postList.clear();
+      for(var individualKey in KEYS)
+      {
+        Posts posts: new Posts
+        (
+          DATA[individualKey]['image'],
+          DATA[individualKey]['description'],
+          DATA[individualKey]['date'],
+          DATA[individualKey]['time'],
+        );
+        postList.add(posts);
+
+
+      }
+      setState(() {
+        print('Length: $postList.length');
+      });
+    });
+  }
   void _logoutUser() async{
     try{
       await widget.auth.signOut();
@@ -33,7 +62,13 @@ class _HomePageState extends State<HomePage>
         title: new Text('Home'),
       ),
       body:  new Container(
+        child: postList.length==0? new Text("No Blog post availabel"): new ListView.builder(
+          itemCount: postList.length,
+          itemBuilder: (_, index){
+            return PostsUI(postList[index].image,postList[index].description,postList[index].date,postList[index].time);
+          }
 
+        ),
       ),
       bottomNavigationBar: new BottomAppBar(
         color: Colors.pink,
@@ -69,6 +104,41 @@ class _HomePageState extends State<HomePage>
             ],
           )
           
+        ),
+      )
+    );
+  }
+  Widget PostsUI(String image, String description,String date, String time){
+    return new Card(
+      elevation:  10.0,
+      margin: EdgeInsets.all(15.0),
+      child: new Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+           new Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: <Widget>[
+              new Text(
+              date,
+              style: Theme.of(context).textTheme.subtitle,
+              textAlign: TextAlign.center,
+            ),
+            new Text(
+              time,
+              style: Theme.of(context).textTheme.subtitle,
+              textAlign: TextAlign.center,
+            )
+           ],),
+           SizedBox(height: 10.0,),
+           new Image.network(image, fit:BoxFit.cover),
+          SizedBox(height: 10.0,),
+            new Text(
+              description,
+              style: Theme.of(context).textTheme.subhead,
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       )
     );
